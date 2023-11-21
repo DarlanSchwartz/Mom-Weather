71984da33e00f71cc6238c5c4b765e64ca2ed8d6
Schwartz <darlanswtz@gmail.com>
2023-11-21 03:05:57 -0300

    Fix: theme context import

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.page";
-import { ThemeContext, ThemeProvider } from "styled-components";
-import { LightColors , DarkColors } from "./styles/Colors";
+import { ThemeProvider } from "styled-components";
+import { LightColors, DarkColors } from "./styles/Colors";
 import { useState } from "react";
-
+import ThemeContext from "./contexts/Theme.context";
 
 export default function App() {
   const [darkModeEnabled, setDarkModeEnabled] = useState(false);
-  function getCurrentColors(){
+  function getCurrentColors() {
     return darkModeEnabled ? DarkColors : LightColors;
   }
   return (
