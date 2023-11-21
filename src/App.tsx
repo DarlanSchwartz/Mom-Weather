9db49ebcb377e33379298e322d2da71818cc32ec
Schwartz <darlanswtz@gmail.com>
2023-11-21 00:57:28 -0300

    Chore: created home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.page";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home/>} />
       </Routes>
     </BrowserRouter>
   )
