ff6919801b2e285bc2a0e9d0df3228c5d1449fa3
Schwartz <darlanswtz@gmail.com>
2023-11-21 00:42:09 -0300

    Chore: router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
-      <div>
-        Wheater
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
