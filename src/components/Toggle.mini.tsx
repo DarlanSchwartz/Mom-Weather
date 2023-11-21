07971ded49c835ef6510b9f2a177a7a6fa58a82a
Schwartz <darlanswtz@gmail.com>
2023-11-21 03:06:50 -0300

    Feat: created toggle component

---
+++ src/components/Toggle.mini.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import React from 'react';
+
+type ToggleProps = {
+    enabled: boolean;
+    onToggle: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function Toggle({ enabled, onToggle }: ToggleProps) {
+    return (
+        <ToggleContainer onClick={() => onToggle(!enabled)}>
+            <ToggleCircle $enabled={enabled} />
+        </ToggleContainer>
+    )
+}
+const ToggleContainer = styled.div`
+    width: 40px;
+    height: 20px;
+    border-radius: 20px;
+    background-color: ${({ theme }) => theme.colors.textLowGray};
+    display: flex;
+    align-items: center;
+    position: relative;
+    cursor: pointer;
+`;
+
+type ToggleCircleProps = {
+    $enabled: boolean;
+}
+
+const ToggleCircle = styled.div<ToggleCircleProps>`
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    background-color: white;
+    
+    position: absolute;
+    left: ${p => p.$enabled ? '55%' : '2px'};
+    top: 50%;
+    transform: translateY(-50%);
+    transition: all 200ms ease;
+`;
